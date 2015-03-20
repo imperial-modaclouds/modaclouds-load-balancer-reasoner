ef2a263c4b53ce3766531b8480cee33442821028
Weikun Wang <weikunwang89@gmail.com>
2015-03-20 12:56:41 +0000

    First the problem of having identical demand

--- bcmpoptim_poc_fmincon.m
+++ bcmpoptim_poc_fmincon.m
@@ -4,7 +4,26 @@ function [fopt, xopt, iter] = bcmpoptim_poc_fmincon(S, N, Z, revenue)
 n = R*M;
 V = zeros(M,R);
 %% initial point
-x0 = init_rand(M,R);
+%x0 = init_rand(M,R);
+x0 = bcmpoptim_prog_heur1(S, revenue);
+x0 = reshape(x0,1,n);
+
+fastTofast = zeros(M,R);
+[x,y] = find(S==min(S(:)));
+if y(1) == 1
+    fastTofast(:,2) = 1/(M-1);
+    fastTofast(x(1),:) = [1,0];
+else
+    fastTofast(:,1) = 1/(M-1);
+    fastTofast(x(1),:) = [0,1];
+end
+temp(1,:) = reshape(fastTofast,1,n);
+
+for i = 2:5
+    temp(i,:) = init_rand(M,R);
+end
+
+x0Set = CustomStartPointSet(temp);
 
 %% options
 MaxCheckIter=1000;
@@ -16,21 +35,26 @@ options.MaxFunEvals = 1e10;
 options.MaxSQPIter = 500;
 options.TolCon = 1e-8;
 options.Algorithm = 'interior-point';
-options.OutputFcn =  @outfun;
+%options.OutputFcn =  @outfun;
 
 XLB = zeros(size(x0)); % lower bounds on x variables
 XUB = ones(size(x0)); % upper bounds on x variables
 
 T0 = tic; % needed for outfun
 %% optimization program
-[x, f, ~, output]=fmincon(@objfun,x0,[],[],[],[],XLB,XUB,@nnlcon,options);
-iter = output.iterations
+ms=MultiStart('Display','iter','UseParallel','always');
+problem=createOptimProblem('fmincon','objective',@objfun,'x0',x0,'lb',XLB,'ub',XUB,'nonlcon',@nnlcon,'options',options);
+[x,~,~,output]=run(ms,problem,x0Set);
+
+%[x, f, ~, output]=fmincon(@objfun,x0,[],[],[],[],XLB,XUB,@nnlcon,options);
+%iter = output.iterations
 V = reshape(x,M,R);
 L = S.*V;
 c = 1;
 %[X] = aql(L,c*N,Z);
 %fopt = -revenue*X'
-fopt = f
+[X] = amvabs(L,c*N,Z);
+fopt = sum(-revenue*X');
 xopt = reshape(x,M,R)
 
     function [c,ceq] = nnlcon(x)
